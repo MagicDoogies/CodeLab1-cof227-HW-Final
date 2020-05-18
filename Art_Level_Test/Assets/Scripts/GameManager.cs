73c0fa412f17ebfe39ac2333eef22d57d6506248
MagicDoogies <37319450+MagicDoogies@users.noreply.github.com>
2020-05-18 11:37:26 -0400

    Created persistent background music.

--- Art_Level_Test/Assets/Scripts/GameManager.cs
+++ Art_Level_Test/Assets/Scripts/GameManager.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour //this script was going to be for a lot of things but now it's only purpose is to not delete the background music everytime the scene changes. 
 {
-   
-
-
+    public static GameManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if (instance == null)
+        { //if instance isn't set
+            instance = this; //set it to this instance
+            DontDestroyOnLoad(gameObject); //Don't destory this gameObject
+        }
+        else
+        { //otherwise, if we have a singleton already
+            Destroy(gameObject); //Destroy this instance
+        }
     }
 
     // Update is called once per frame
